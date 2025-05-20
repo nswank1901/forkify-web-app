56e1aaff6c21a6055cdcc1d44def4c4379f38070
nswank1901 <93402347+nswank1901@users.noreply.github.com>
2025-05-20 09:57:47 -0400

    Fix env variable

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,5 @@ export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = process.env.API_KEY;
+// export const KEY = import.meta.env.API_KEY;
 export const MODAL_CLOSE_SEC = 2.5;
